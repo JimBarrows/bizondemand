325e4177e77bb8be699a043fdc4c45c57ef7128a
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-11-16 07:10:45 -0700

    Made the plural and singles better, and added a default now for from dates.

--- party/models/Party.py
+++ party/models/Party.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 class Party(models.Model):
 	def __unicode__(self):
@@ -15,6 +16,7 @@ class Person( Party):
 		return self.firstName + ' ' + self.middleName + ' '+ self.lastName
 	class Meta:
 		app_label = 'party'
+		verbose_name_plural = 'People'
 
 class Organization( Party):
 	name=models.CharField(max_length=128)
@@ -22,6 +24,7 @@ class Organization( Party):
 		return self.name
 	class Meta:
 		app_label = 'party'
+		verbose_name_plural = 'Organizations'
 
 class PartyType(models.Model):
 	description = models.CharField(max_length=250)
@@ -31,16 +34,19 @@ class PartyType(models.Model):
 		return self.description
 	class Meta:
 		app_label = 'party'
+		verbose_name_plural = 'Party Types'
 
 class PartyClassification(models.Model):
 	party = models.ForeignKey(Party)
 	partyType = models.ForeignKey(PartyType)
-	fromDate = models.DateField()
+	fromDate = models.DateField(default = datetime.today())
 	thruDate = models.DateField(blank = True, null = True)
 	def __unicode__(self):
 		return self.partyType.description
 	class Meta:
 		app_label = 'party'
+		verbose_name_plural = 'Party Classifications'
+		verbose_name = 'Party Classification'
 
 class PartyRoleType( models.Model):
 	description = models.CharField(max_length=250)
@@ -50,16 +56,20 @@ class PartyRoleType( models.Model):
 		return self.description
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Party Role Type'
+		verbose_name_plural = 'Party Role Types'
 
 class PartyRole(models.Model):
 	party = models.ForeignKey(Party)
 	partyRoleType = models.ForeignKey(PartyRoleType)
-	fromDate = models.DateField()
+	fromDate = models.DateField(default = datetime.today())
 	thruDate = models.DateField(blank = True, null = True)
 	def __unicode__(self):
 		return self.partyRoleType.description
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Party Role'
+		verbose_name_plural = 'Party Roles'
 
 class PartyRelationshipType(models.Model):
 	name = models.CharField(max_length=250)
@@ -71,11 +81,13 @@ class PartyRelationshipType(models.Model):
 		return self.name
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Party Relationship Type'
+		verbose_name_plural = 'Party Relationship Types'
 
 class PartyRelationship(models.Model):
 	comment = models.TextField()
 	relationshipType = models.ForeignKey(PartyRelationshipType)
-	fromDate = models.DateField()
+	fromDate = models.DateField( default = datetime.today())
 	thruDate = models.DateField(blank = True, null = True)
 	fromRole = models.ForeignKey(PartyRole, related_name='from_role_set')
 	toRole = models.ForeignKey(PartyRole, related_name='to_role_set')
@@ -85,17 +97,21 @@ class PartyRelationship(models.Model):
 		return self.comment
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Party Relationship'
+		verbose_name_plural = 'Party Relationships'
 
 class PartyPostalAddress(models.Model):
 	party = models.ForeignKey(Party)
 	comment = models.TextField()
-	fromDate = models.DateField()
+	fromDate = models.DateField( default = datetime.today())
 	thruDate = models.DateField(blank = True, null = True)
 	location = models.ForeignKey('PostalAddress')
 	def __unicode__(self):
 		return self.location.street1
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Party Postal Address'
+		verbose_name_plural = 'Party Postal Addresss'
 
 class PriorityType(models.Model):
 	description = models.CharField(max_length=250)
@@ -103,6 +119,8 @@ class PriorityType(models.Model):
 		return self.description
 	class Meta:
 		app_label = 'party'
+		verbose_name = 'Priority Type '
+		verbose_name_plural = 'Priority Types'
 
 class StatusType(models.Model):
 	description = models.CharField(max_length=250)
@@ -110,5 +128,5 @@ class StatusType(models.Model):
 		return self.description
 	class Meta:
 		app_label = 'party'
-	class Meta:
-		app_label = 'party'
+		verbose_name = 'Status Type '
+		verbose_name_plural = 'Status Types'
