6250fdffb438462ef142c75b2d2c94a1c722e014
Jim Barrows <Jim.Barrows@gmail.com[color]>
2010-11-15 23:10:34 -0700

    products added

--- settings.py
+++ settings.py
@@ -97,5 +97,6 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'bizondemand.party'
+    'bizondemand.party',
+    'bizondemand.products',
 )
